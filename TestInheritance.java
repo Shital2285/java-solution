b36f91cc11cef012bdde23cf8c8b1896ea584174
Shital Hanumant Bagal <shitalbagal2285@gmail.com>
2025-05-12 18:43:46 +0530

    Add files via upload

---
+++ TestInheritance.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TestInheritance
+{
+public static void main(String args[])
+  {
+	  Derived d=new Derived();
+	  d.accpet();
+	  d.getThirdNo();
+	  d.dispMax();
+	  d.dispMin();
+	  d.equal();
+  }
+ 
+} 
