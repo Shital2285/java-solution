30e7fd1476131a949aa4141d14ed2ec59f6192ec
Shital Hanumant Bagal <shitalbagal2285@gmail.com>
2025-05-12 18:58:12 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+import java.io.*;
+
+class Triangle
+{
+  public static void main(String args[])
+{
+  int i,j,n;
+   Scanner sc=new Scanner(System.in);
+   System.out.println(" Enter the number");
+   n=sc.nextInt();
+   for(i=0; i<n; i++)
+   {
+    System.out.print(" * ");
+    for(j=1; j<=i; j++)
+    {
+    System.out.print(" * ");
+    }
+     System.out.println("");
+            
+  }
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
