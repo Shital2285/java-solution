86e0c73e20213e3c75f8434caf3760d06cf9dca3
Shital Hanumant Bagal <shitalbagal2285@gmail.com>
2025-05-12 18:41:52 +0530

    Add files via upload

---
+++ TestBiscuit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TestBiscuit
+{
+public static void main(String args[])
+  {
+	  Retailer r=new Retailer();
+	  r.accpet();
+	  r.getData();
+	  r.total();
+	  r.display();
+	   
+  }
+ 
+} 
