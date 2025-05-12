f1fa8faad9ecfed40a3d9a7506d6355e301f91b0
Shital Hanumant Bagal <shitalbagal2285@gmail.com>
2025-05-12 18:48:43 +0530

    Add files via upload

---
+++ Diamond1.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.io.*;
+
+class Pyramid1
+{
+  public static void main(String args[])
+  {
+	 Scanner sc=new Scanner(System.in);
+	 int row;
+	 int i,j;
+	 System.out.println("enter the rows");
+	 row=sc.nextInt();
+	 for(i=0;i<row;i++)
+	 {
+		 for (j=0;j<row;j++)
+		 {
+			 System.out.println("* ");
+			 
+		 }
+	 }
+		 
+	 
+	 
+ 
+  }
+
+}
